9ad1e5c9b207e0880a128db89c01cd9b72e9ce4d
Minh Hieu <minhhieudoqb255@gmail.com>
2025-06-26 17:59:50 +0700

    change-status

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const bodyPaser = require("body-parser");
 const database = require("./config/database");
 
 const Task = require('./api/v1/model/task.model')
@@ -10,6 +11,8 @@ database.connect();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(bodyPaser.json());
 
 route(app);
 
