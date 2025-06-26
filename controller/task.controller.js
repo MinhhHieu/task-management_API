5a22826bec628fb881de650f311c8d6b3341fa7d
Minh Hieu <minhhieudoqb255@gmail.com>
2025-06-26 15:20:24 +0700

    sort by criteria

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -7,10 +7,19 @@ module.exports.index = async (req, res) => {
   };
 
   if (req.query.status) {
-    find.status = req.query.status
+    find.status = req.query.status;
   }
 
-  const tasks = await Task.find(find);
+  console.log(req.query);
+  // Sort
+  const sort = {};
+
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  };
+  // End Sort
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 };
