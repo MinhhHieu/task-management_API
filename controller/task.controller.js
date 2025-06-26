0c74a07d402ce625a68b2fcd9aac3587495011f1
Minh Hieu <minhhieudoqb255@gmail.com>
2025-06-26 15:01:34 +0700

    filter by status

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -6,9 +6,9 @@ module.exports.index = async (req, res) => {
     deleted: false,
   };
 
-//   if (req.query.status) {
-//     find.status = req.query.status
-//   }
+  if (req.query.status) {
+    find.status = req.query.status
+  }
 
   const tasks = await Task.find(find);
 
