86fb5d95357d0203a74f544d12a6f6e24e4dae67
Minh Hieu <minhhieudoqb255@gmail.com>
2025-07-08 14:39:24 +0700

    change route to private

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -6,6 +6,8 @@ const controller = require("../controller/user.controller.js");
 const registerPostValidate = require("../validates/user.validate"); 
 const loginPostValidate = require("../validates/user.validate");
 
+const authMiddleware = require("../middlewares/auth.middleware");
+
 router.post('/register', registerPostValidate.registerPost,controller.register);
 
 router.post('/login', loginPostValidate.loginPost,controller.login);
@@ -16,6 +18,6 @@ router.post('/password/otp', controller.otpPassword);
 
 router.post('/password/reset', controller.resetPassword);
 
-router.get('/information', controller.informationUser);
+router.get('/information', authMiddleware.requireAuth, controller.informationUser);
 
 module.exports = router;
